2c29ec338b929d13e10b6d97f63e8c0be3a56c9f
lc <1033581609@qq.com>
2022-05-08 20:40:51 +0800

    add: 多进程执行命令

--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,33 +1,34 @@
-"use strict"
+'use strict'
 
-const path = require("path")
-const Package = require("@iacg-cli/package")
-const log = require("@iacg-cli/log")
+const path = require('path')
+const Package = require('@iacg-cli/package')
+const log = require('@iacg-cli/log')
+const cp = require('child_process')
 
 const SETTINGS = {
-  init: "@iacg-cli/init",
+  init: '@iacg-cli/init',
 }
 
-const CACHE_DIR = "dependencies"
+const CACHE_DIR = 'dependencies'
 
 async function exec() {
   let targetPath = process.env.CLI_TARGET_PATH
   const homePath = process.env.CLI_HOME_PATH
-  let storeDir = ""
+  let storeDir = ''
   let pkg
-  log.verbose("targetPath", targetPath)
-  log.verbose("homePath", homePath)
+  log.verbose('targetPath', targetPath)
+  log.verbose('homePath', homePath)
 
   const cmdObj = arguments[arguments.length - 1]
   const cmdName = cmdObj.name()
   const packageName = SETTINGS[cmdName]
-  const packageVersion = "latest"
+  const packageVersion = 'latest'
 
   if (!targetPath) {
     targetPath = path.resolve(homePath, CACHE_DIR) // 生成缓存路径
-    storeDir = path.resolve(targetPath, "node_modules")
-    log.verbose("targetPath", targetPath)
-    log.verbose("storeDir", storeDir)
+    storeDir = path.resolve(targetPath, 'node_modules')
+    log.verbose('targetPath', targetPath)
+    log.verbose('storeDir', storeDir)
     pkg = new Package({
       targetPath,
       storeDir,
@@ -48,18 +49,47 @@ async function exec() {
       packageVersion,
     })
   }
-  
+
   const rootFile = pkg.getRootFilePath()
+  log.verbose('rootFile', rootFile)
+
   if (rootFile) {
     try {
       // 在当前进程中调用
-      require(rootFile).call(null, Array.from(arguments));
+      // require(rootFile).call(null, Array.from(arguments))
+      // 在node子进程中调用
+      const args = Array.from(arguments)
+      const cmd = args[args.length - 1]
+      const object = Object.create(null)
+      Object.keys(cmd).forEach(key => {
+        if (
+          cmd.hasOwnProperty(key) &&
+          !key.startsWith('_') &&
+          key !== 'parent'
+        ) {
+          object[key] = cmd[key]
+        }
+      })
+      args[args.length - 1] = object
+      const code = `require('${rootFile}').call(null, ${JSON.stringify(args)})`
 
+      const child = cp.spawn('node', ['-e', code], {
+        cwd: process.cwd(),
+        stdio: 'inherit',
+      })
+
+      child.on('error', e => {
+        log.error(e.message)
+        process.exit(1)
+      })
+      child.on('exit', e => {
+        log.verbose('命令执行成功:' + e)
+        process.exit(e)
+      })
     } catch (e) {
       log.error(e.message)
     }
   }
 }
-
 
 module.exports = exec
